e250d400802a11f5d0fc73c970f0146a9d739c51
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-11-20 10:17:13 -0500

    Update odd.js

--- odd.js
+++ odd.js
@@ -3,21 +3,23 @@ const readlineSync = require("readline-sync");
 let sum = 0;
 let value = 0;
 
+console.log("");
+
 while (value <= 0) {
-value = readlineSync.question("Positive Integer: "); 
+value = readlineSync.question("Positive integer: ");
 let num = value;
 while ( num > 0 )
 {
 
-let n = num % 10 ; 
+let n = num % 10 ;
 if( n % 2 != 0)
- 
+
 {
 
- sum = sum + n; 
+ sum = sum + n;
 }
 
- num = parseInt( num / 10 ); 
+ num = parseInt( num / 10 );
 }
 
 }
