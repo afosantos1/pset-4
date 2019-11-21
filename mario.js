3b888961189c034a93069319ae2e76e1017b9661
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-11-20 21:20:54 -0500

    Create mario.js

---
+++ mario.js
@@ -0,0 +1,21 @@
+const input = require("readline-sync");
+
+let height = Number(input.question("\nHeight: "));
+let output = [];
+
+while (height > 24 || height < 1 || Number.isNaN(height) == true || height%1!=0){
+  height = Number(input.question("Height: "));
+}
+
+console.log();
+
+for(let x = 1; x < height+1; x++){
+  for(var i = 0; i < height-x; i++){
+    output.push(" ");
+  }for(var i = 0; i < x + 1; i++){
+    output.push("#");
+  }
+  
+  console.log(output.join(""));
+  output = [];
+}
